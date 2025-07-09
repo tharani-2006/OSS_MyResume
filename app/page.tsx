68852dee3ee1b5e3a8d2483d5a1bad1ba3335c95
Siva <vsivareddy.venna@gmail.com>
2025-07-09 22:21:57 +0530

    Remove EmailJSDebug component and debug button completely

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ChatBot from "./components/ChatBot";
-import EmailJSDebug from "./components/EmailJSDebug";
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -43,7 +42,6 @@ export default function Home() {
       <Contact />
       <Footer />
       <ChatBot />
-      <EmailJSDebug />
     </main>
   );
 }
